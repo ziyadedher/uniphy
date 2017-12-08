c032e21348f26683ea4af895caab524fc01a0be5
Ziyad Edher <ziyad.edher@gmail.com>
2017-12-08 13:48:24 -0500

    Add physical object drawing skeleton

--- src/drawing.py
+++ src/drawing.py
@@ -1,8 +1,25 @@
+# This file is part of UniPhy.
+#
+# UniPhy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# UniPhy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with UniPhy.  If not, see <http://www.gnu.org/licenses/>.
+
 """Manages all forms of drawing from objects to text.
 """
-from typing import Tuple
+from typing import Tuple, List
 
 import arcade
+
+from src import physics
 
 
 # Window constants
@@ -19,7 +36,7 @@ class Window(arcade.Window):
     """
     def __init__(self, width: int, height: int, title: str,
                  background_color: Tuple[int, int, int]) -> None:
-        """Initializes this window.
+        """Initialize this window.
         """
         # Initializes the arcade screen and sets background color
         # TODO: implement resizing
@@ -81,4 +98,36 @@ class Window(arcade.Window):
         """Called on any key up.
         """
         # TODO: call all key release actions
+        pass
+
+
+class ObjectDrawer:
+    """Manages all object drawing to the window.
+    """
+    # === Private Attributes ===
+    # _objects:
+    #   list of all physics objects
+    _objects: List[physics.PhysicalObject]
+
+    def __init__(self) -> None:
+        """Initializes this drawer.
+        """
+        self._objects = []
+
+    def register_object(self, object: physics.PhysicalObject) -> None:
+        """Register a physics object into the drawer.
+        """
+        # TODO: implement
+        pass
+
+    def purge_inactive_objects(self) -> None:
+        """Remove all inactive objects from drawing.
+        """
+        # TODO: implement
+        pass
+
+    def draw(self) -> None:
+        """Draws all objects.
+        """
+        # TODO: implement
         pass
